9aef323643d911b3e8190dc299f571f01b17f36e
Preeti788 <kumaripreetispj@gmail.com>
2025-08-02 23:22:25 +0530

    Time: 6 ms (27.14%) | Memory: 41.6 MB (84.52%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        int n = nums.length;
+
+        if(n == 0) return result;
+
+        int start = nums[0];
+
+        for(int i=1; i<n; i++){
+            if(nums[i] != nums[i-1]+1){
+                if(start == nums[i-1]){
+                    result.add(String.valueOf(start));
+                }else{
+                    result.add(start + "->" + nums[i-1]);
+                }
+
+                start = nums[i];
+            }
+        }
+        if(start == nums [n-1]){
+            result.add(String.valueOf(start));
+        }else{
+            result.add(start + "->" + nums[n-1]);
+        }
+
+        return result;
+        
+    }
+}
