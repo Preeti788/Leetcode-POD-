06d34758ac551bf927548f40d4c043f6b71dc1c0
Preeti788 <kumaripreetispj@gmail.com>
2025-08-02 23:27:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (92.62%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -3,28 +3,27 @@ class Solution {
         List<String> result = new ArrayList<>();
         int n = nums.length;
 
-        if(n == 0) return result;
+        if (n == 0) return result;
 
         int start = nums[0];
 
-        for(int i=1; i<n; i++){
-            if(nums[i] != nums[i-1]+1){
-                if(start == nums[i-1]){
-                    result.add(String.valueOf(start));
-                }else{
-                    result.add(start + "->" + nums[i-1]);
-                }
-
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1] + 1) {
+                // Build the string more efficiently
+                result.add(buildRange(start, nums[i - 1]));
                 start = nums[i];
             }
         }
-        if(start == nums [n-1]){
-            result.add(String.valueOf(start));
-        }else{
-            result.add(start + "->" + nums[n-1]);
-        }
 
+
+        result.add(buildRange(start, nums[n - 1]));
         return result;
-        
+    }
+
+    private String buildRange(int start, int end) {
+        if (start == end) return Integer.toString(start);
+        StringBuilder sb = new StringBuilder();
+        sb.append(start).append("->").append(end);
+        return sb.toString();
     }
 }
