a67f65bc37fc885c5219dd56f6eb9551da989a2f
Preeti788 <kumaripreetispj@gmail.com>
2025-08-02 23:03:09 +0530

    Time: 42 ms (91.55%) | Memory: 67.4 MB (36.62%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.java
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -1,32 +1,32 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
-    
-        Map<Integer, Integer> count = new TreeMap<>();
-        int n = basket1.length;
-    
-        for (int val : basket1) count.put(val, count.getOrDefault(val, 0) + 1);
-        for (int val : basket2) count.put(val, count.getOrDefault(val, 0) - 1);
+        Map<Integer, Integer> freq = new HashMap<>();
+        int minVal = Integer.MAX_VALUE;
+
+        for (int x : basket1) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+            minVal = Math.min(minVal, x);
+        }
+
+        for (int x : basket2) {
+            freq.put(x, freq.getOrDefault(x, 0) - 1);
+            minVal = Math.min(minVal, x);
+        }
 
         List<Integer> extra = new ArrayList<>();
-        long minElement = Long.MAX_VALUE;
-
-        for (int key : count.keySet()) {
-            int freq = count.get(key);
-
-            if (freq % 2 != 0) return -1; 
-
-            int half = Math.abs(freq) / 2;
-            for (int i = 0; i < half; i++) {
-                extra.add(key);
+        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
+            int count = e.getValue();
+            if (count % 2 != 0) return -1;
+            for (int i = 0; i < Math.abs(count) / 2; i++) {
+                extra.add(e.getKey());
             }
-            minElement = Math.min(minElement, key);
         }
 
         Collections.sort(extra);
-
         long cost = 0;
-        for (int i = 0; i < extra.size() / 2; i++) {
-            cost += Math.min(extra.get(i), 2 * minElement);
+        int m = extra.size();
+        for (int i = 0; i < m / 2; i++) {
+            cost += Math.min(extra.get(i), 2 * minVal);
         }
 
         return cost;
