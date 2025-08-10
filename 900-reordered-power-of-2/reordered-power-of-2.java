e95c86f81f56186d77f0190d2bc93f4e92c42197
Preeti788 <kumaripreetispj@gmail.com>
2025-08-10 22:21:08 +0530

    Time: 1 ms (85.79%) | Memory: 40.8 MB (55.26%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+    
+        String target = countDigits(n);
+        for (int i = 0; i < 31; i++) { 
+            if (target.equals(countDigits(1 << i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private String countDigits(int num) {
+        char[] arr = String.valueOf(num).toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+}
