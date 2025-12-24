13befeca93250a990a9c8f239bbab0c39517c1d7
Preeti788 <kumaripreetispj@gmail.com>
2025-12-24 22:13:39 +0530

    Time: 2 ms (99.87%) | Memory: 46.7 MB (25.28%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,33 +1,34 @@
-import java.util.*;
-
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
 
-        int total = 0;
+        int result = 0;
+        int prev = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            int current = map.getOrDefault(s.charAt(i), -1);
-            int next = (i + 1 < s.length()) ? map.getOrDefault(s.charAt(i + 1), -1) : 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = getValue(s.charAt(i));
 
-            if (current == -1) {
-                throw new IllegalArgumentException("Invalid Roman character: " + s.charAt(i));
-            }
-
-            if (current < next) {
-                total -= current;
+            if (curr < prev) {
+                result -= curr;
             } else {
-                total += current;
+                result += curr;
             }
+
+            prev = curr;
         }
 
-        return total;
+        return result;
+    }
+
+    private int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
     }
 }
