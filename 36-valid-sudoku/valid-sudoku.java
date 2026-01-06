3255a1c6c7a6ebe5af759c6660c5c48621532918
Preeti788 <kumaripreetispj@gmail.com>
2026-01-06 23:47:44 +0530

    Time: 12 ms (15.68%) | Memory: 52.7 MB (7.85%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,37 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-    
-        Set<Character>[] rows = new HashSet[9];
-        Set<Character>[] cols = new HashSet[9];
-        Set<Character>[] boxes = new HashSet[9];
-        
+
+
+        HashSet<String> set = new HashSet<>();
+
         for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
+            for (int j = 0; j < 9; j++) {
+                char ch = board[i][j];
 
-    
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                char val = board[r][c];
-                if (val == '.') continue; // Skip empty cells
-
-            
-                if (rows[r].contains(val)) return false;
-                rows[r].add(val);
-
-        
-                if (cols[c].contains(val)) return false;
-                cols[c].add(val);
-
-
-                int boxIndex = (r / 3) * 3 + (c / 3);
-                if (boxes[boxIndex].contains(val)) return false;
-                boxes[boxIndex].add(val);
+                if (ch != '.') {
+                    if (!set.add(ch + " in row " + i) ||
+                        !set.add(ch + " in col " + j) ||
+                        !set.add(ch + " in box " + (i / 3) + "-" + (j / 3))) {
+                        return false;
+                    }
+                }
             }
         }
-
         return true;
     }
 }
